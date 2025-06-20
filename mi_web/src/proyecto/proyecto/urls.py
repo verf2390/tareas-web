3c79f7bcdc5f69de24d8a4e1326564835753fe19
verf2390 <149746488+verf2390@users.noreply.github.com>
2025-06-20 11:45:45 -0400

    modificacion de  urls.py

--- mi_web/src/proyecto/proyecto/urls.py
+++ mi_web/src/proyecto/proyecto/urls.py
@@ -1,27 +1,21 @@
-﻿"""
-URL configuration for proyecto project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
+from .views import lista_pendientes
+from .views import detalle_tarea
+from .views import crear_tarea
+from .views import editar_tarea
+from .views import eliminar_tarea
+from .views import logueo, pagina_registro
+from django.contrib.auth.views import LogoutView
+from . import views
 
-def inicio(request):
-    return HttpResponse("🎉 ¡Servidor Django funcionando correctamente!")
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', inicio),  # Ruta principal (http://IP:8000)
+urlpatterns = [path('', lista_pendientes.as_view(), name ='tareas'),
+               path('login/', logueo.as_view(), name ='login'),
+               path('registro/', pagina_registro.as_view(), name ='registro'),
+               path('logout/', LogoutView.as_view(next_page='login'), name ='logout'),
+               path('tarea/<int:pk>', detalle_tarea.as_view(), name ='tarea'),
+               path('crear_tarea/', crear_tarea.as_view(), name ='crear_tarea'),
+               path('editar_tarea/<int:pk>', editar_tarea.as_view(), name ='editar_tarea'),
+               path('eliminar_tarea/<int:pk>', eliminar_tarea.as_view(), name ='eliminar_tarea'),
+               path('toggle-completo/<int:pk>/', views.toggle_completo, name='toggle_completo'),
+               ]
 ]
